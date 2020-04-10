944486543d9c6ad5c5b5ab89af233c809e44dedb
Geoff Boeing <boeing@usc.edu>
2020-04-10 09:38:58 -0700

    verbose

--- win/conda-base-update.bat
+++ win/conda-base-update.bat
@@ -2,5 +2,5 @@ CALL conda update --all --force-reinstall --yes
 CALL conda clean --all --yes
 CALL conda init --all
 CALL conda env export -n base > environment.yml
-CALL git add environment.yml --verbose
+CALL git add environment.yml -v
 CALL git commit -m "update windows conda base environment"
