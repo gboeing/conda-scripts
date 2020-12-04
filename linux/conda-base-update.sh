a62696a97b3c409090df6beb9ac73151ba5765f4
Geoff Boeing <boeing@usc.edu>
2020-12-03 16:41:24 -0800

    add repack

--- linux/conda-base-update.sh
+++ linux/conda-base-update.sh
@@ -6,3 +6,4 @@ conda init --all
 conda env export -n base > environment.yml
 git add environment.yml -v
 git commit -m "update linux conda base environment"
+git repack -a -d -f --depth=250 --window=250
