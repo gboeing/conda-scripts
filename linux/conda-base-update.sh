4626af09327635d13976ee618f637adb04e83fa2
Geoff Boeing <boeing@usc.edu>
2021-04-15 20:46:07 -0700

    updates

--- linux/conda-base-update.sh
+++ linux/conda-base-update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 conda clean --all --yes
-mamba update --all --force-reinstall --strict-channel-priority --yes
+conda update --all --force-reinstall --strict-channel-priority --yes
 conda clean --all --yes
 conda init --all
 conda env export -n base > environment.yml
